45b60971f9923bd5cebb1d837e3de6611d3e121a
Martin <martinsung00@gmail.com>
2020-08-18 15:33:28 -0700

    Complete remove duplicates

--- AdvancedPrep-LevelOne/02-linked-lists/exercises/02-remove-duplicates.js
+++ AdvancedPrep-LevelOne/02-linked-lists/exercises/02-remove-duplicates.js
@@ -3,4 +3,19 @@
 /*
   Implement an algorithm that removes duplicate values in a linked list using only O(1) space.
 */
-export function removeDuplicates(head) {}
+export function removeDuplicates(head) {
+  let currentNode = head;
+  let nextNode = head.next;
+
+  if (nextNode === null) {
+    return "Only one item exits in the data structure";
+  }
+  while (nextNode !== null) {
+    if (currentNode.data === nextNode.data) {
+      nextNode = nextNode.next;
+      break;
+    }
+    currentNode = nextNode;
+    nextNode = nextNode.next;
+  }
+};
