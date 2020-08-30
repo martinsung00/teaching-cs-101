1d6e397aa7bcffea607e70e13c0a53be44181978
zwilhelmm <13949359+zwilhelmm@users.noreply.github.com>
2020-08-29 23:45:51 -0700

    Add bubbleSort test reference

---
+++ AdvancedPrep-LevelOne/04-searching-and-sorting/solutions/01-bubble-sort.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/*
+  Time: O(n^2)
+  Space: O(1)
+*/
+
+export function bubbleSort(list) {
+  for (let i = 0; i < list.length; i++) {
+    for (let j = 0; j < list.length - i - 1; j++) {
+      if (list[j] > list[j + 1]) {
+        const temp = list[j];
+        list[j] = list[j + 1];
+        list[j + 1] = temp;
+      }
+    }
+  }
+
+  return list;
+}
